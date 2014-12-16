ff4d2621e4e9edcc0a2390d33e6b99749ac6a839
Jordan Liu <jminjie@umich.edu>
2014-12-15 23:03:09 -0500

    interesting bug pre-refactor
    In the process of refactoring to OO style (like in article example)

--- client.go
+++ client.go
@@ -5,26 +5,60 @@ import (
   "net"
   "bufio"
   "os"
+  "strings"
 )
 
-const myip = "192.168.56.1"
+const serverIP = "192.168.56.1"
 
 func main() {
-  conn, err := net.Dial("tcp", myip + ":6666")
+  conn, err := net.Dial("tcp", serverIP + ":6666")
   if err != nil {
-    fmt.Printf("Error dialing\n")
+    fmt.Println("Error dialing")
     return
   }
-  fmt.Printf("Enter username: ")
-  for {
+  
+  quit := make(chan bool)
+  // Send messages
+  go func() {
+    fmt.Printf("Enter username: ")
     reader := bufio.NewReader(os.Stdin)
-    message, err := reader.ReadString('\n')
+    username, err := reader.ReadString('\n')
     if err != nil {
-      fmt.Printf("Error in input\n")
-      return
+      fmt.Println("Invalid username")
     }
-    if len(message) > 0 {
-      fmt.Fprintf(conn, message)
+    conn.Write([]byte(username))
+    for {
+      //fmt.Printf("You: ")
+      reader := bufio.NewReader(os.Stdin)
+      message, err := reader.ReadString('\n')
+      if err != nil {
+        fmt.Println("\nExiting")
+        quit <- true
+      }
+      conn.Write([]byte(message))
     }
+  }()
+  // Receive messages
+  go func() {
+    receivedMessageBuffer := make([]byte, 1024)
+    for i := 0; i < 1024; i++ {
+      receivedMessageBuffer[i] = ' '
+    }
+    _, err = conn.Read([]byte(receivedMessageBuffer))
+    for err == nil {
+      receivedMessage := strings.TrimSpace(string(receivedMessageBuffer))
+      fmt.Println(receivedMessage)
+      for i := 0; i < 1024; i++ {
+        receivedMessageBuffer[i] = ' '
+      }
+      _, err = conn.Read([]byte(receivedMessageBuffer))
+    }
+    if err != nil {
+      fmt.Println("\nExiting")
+      quit <- true
+    }
+  }()
+  if ( <- quit) {
+    return
   }
 }
--- testchat.go
+++ testchat.go
@@ -9,24 +9,50 @@ import(
 func main() {
   listener, err := net.Listen("tcp", ":6666")
   if err != nil {
-    fmt.Println("Error accepting connection\n")
+    fmt.Println("Error accepting connection")
   }
   
+  
+  outgoingMessages := make(chan string)
   for {
     conn, err := listener.Accept()
     if err != nil {
-      fmt.Println("Error accepting connection\n")
+      fmt.Println("Error accepting connection")
     }
-    fmt.Println("Accepted a connection\n")
-    userbuffer := make([]byte, 10)
-    conn.Read(userbuffer)
-    user := strings.Fields(string(userbuffer))
-    //fmt.Println(user)
+    fmt.Println("Accepted a connection")
+    userBuffer := make([]byte, 10)
+    for i := 0; i < 10; i++ {
+      userBuffer[i] = ' '
+    }
+    conn.Read(userBuffer)
+    user := strings.Fields(string(userBuffer))
+    username := user[0]
+    buffer := make([]byte, 1024)
+    for i := 0; i < 1024; i++ {
+      buffer[i] = ' '
+    }
+    // Receive and print messages
+    go func() {
+      _, err := conn.Read(buffer)
+      for err == nil {
+        message := username + ": " + strings.TrimSpace(string(buffer))
+        fmt.Println(message)
+        outgoingMessages <- message
+        for i := 0; i < 1024; i++ {
+          buffer[i] = ' '
+        }
+        _, err := conn.Read(buffer)
+        if err != nil {
+          fmt.Println("Ending connection with", username)
+          return
+        }
+      }
+      fmt.Println("Exiting")
+    }()
+    // Relay messages to clients
     go func() {
       for {
-        buffer := make([]byte, 80)
-        conn.Read(buffer)
-        fmt.Println(user[0] + ":", strings.TrimSpace(string(buffer)))
+        conn.Write([]byte(<- outgoingMessages))
       }
     }()
   }
